f3456bf12310062dfe63310c47384d98e6c7c4d7
aaronmccormack15533 <maron.accormack@gmail.com>
2020-01-23 19:27:28 +0000

    added navbar

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import {
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav,
+	NavItem,
+	NavLink,
+	Container
+} from "reactstrap";
+
+class AppNavbar extends Component {
+	state = {
+		isOpen: false
+	};
+	toggle = () => {
+		this.setState({
+			isOpen: !this.state.isOpen
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<Navbar color="dark" dark expand="sm" class="mb-5">
+					<Container>
+						<NavbarBrand href="/">ShoppingList</NavbarBrand>
+						<NavbarToggler onClick={this.toggle}></NavbarToggler>
+						<Collapse isOpen={this.state.isOpen} navbar>
+							<Nav class="ml-auto" navbar>
+								<NavItem>
+									<NavLink href="https://github.com/aaronmccormack15533">
+										Github
+									</NavLink>
+								</NavItem>
+							</Nav>
+						</Collapse>
+					</Container>
+				</Navbar>
+			</div>
+		);
+	}
+}
+
+export default AppNavbar;
